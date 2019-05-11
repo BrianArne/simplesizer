3178ca8dd175bc51c694c94b172788f491fbdf1f
Brian Arne <baarne@g.cofc.edu>
2019-05-10 21:07:24 -0400

    Figured out sine issue. Gain related

--- Sine/sinetxt.cpp
+++ Sine/sinetxt.cpp
@@ -8,31 +8,28 @@
 #define M_PI (3.141592654)
 #endif
 
-enum {ARG_NAME, ARG_NSAMPS, ARG_FREQ, ARG_SR, ARG_NARGS};
+enum {ARG_NAME, ARG_DUR, ARG_FREQ, ARG_SR, ARG_NARGS};
 
 int main(int argc, char** argv){
-  int j;
-  int i, nsamps;
+  int i, nsamps, dur;
   double samp, freq, srate;
   double twopi = 2.0 * M_PI;
   double angleincr;
 
   if(argc != ARG_NARGS){
-    fprintf(stderr, "usage: sinetext nsamps freq srate\n");
+    fprintf(stderr, "usage: sinetext dur freq srate\n");
     return 1;
   }
-  nsamps = atoi(argv[ARG_NSAMPS]);
+  dur = atoi(argv[ARG_DUR]);
   freq = atoi(argv[ARG_FREQ]);
   srate = atoi(argv[ARG_SR]);
+  nsamps = (int) (dur * srate);
 
   // Make one complete cycle
-  angleincr = twopi * freq/srate;
-  while (j < 1000 ){
-    for (i = 0; i < nsamps; i++){
-      samp = sin(angleincr * i);
-      fprintf(stdout, "%.8lf\t%.8lf\n", samp, samp*samp);
-    }
-    j++;
+  angleincr = twopi*(freq/srate);
+  for (i = 0; i < nsamps; i++){
+    samp = sin(angleincr * i);
+    fprintf(stdout, "%.8lf\n", samp);
   }
   fprintf(stderr, "done\n");
   return 0;
